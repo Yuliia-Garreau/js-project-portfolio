29b46256a46cfcc261dfef44ca4e69ba9db92d0a
MAKCIMELIANO <makcimeliano@gmail.com>
2024-12-20 22:09:45 +0200

    edit

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,14 @@
 console.log('Hello from header.js');
 
-const dropdownLink = document.querySelector('.header-menu a');
-const dropdownList = document.querySelector('.header-menu ul');
+// const dropdownLink = document.querySelector('.header-menu a');
+// const dropdownList = document.querySelector('.header-menu ul');
 
-dropdownLink.addEventListener('click', () => {
-  dropdownList.classList.toggle('show');
-});
+// dropdownLink.addEventListener('click', () => {
+//   dropdownList.classList.toggle('show');
+// });
 
-document.addEventListener('click', e => {
-  if (!dropdownLink.contains(e.target) && !dropdownList.contains(e.target)) {
-    dropdownList.classList.remove('show');
-  }
-});
+// document.addEventListener('click', e => {
+//   if (!dropdownLink.contains(e.target) && !dropdownList.contains(e.target)) {
+//     dropdownList.classList.remove('show');
+//   }
+// });
