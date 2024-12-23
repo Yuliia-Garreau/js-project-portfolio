0e0485dd60353d2ed476806dd79e174dca02a544
Yuliia Garreu <yuliia.garreau@gmail.com>
2024-12-23 00:31:40 +0000

    320 screen issue
    I still have a problem to adapt projects section for 320px screens. If I add max-width: 320px; for projects-container it is in conflict with the swiper.

--- src/js/projects.js
+++ src/js/projects.js
@@ -14,6 +14,8 @@ import 'swiper/css/navigation';
 // const prevBtn = document.querySelector(".swiper-button-prev");
 const next = document.querySelector(".next-slide");
 const prev = document.querySelector(".prev-slide");
+const arrowNext = document.querySelector(".arrow-narrow-next");
+const arrowPrev = document.querySelector(".arrow-narrow-prev");
 
 
 //     // // spaceBetween: 10,
@@ -47,23 +49,30 @@ const swiper = new Swiper('.mySwiper', {
             reachEnd() {
               next.disabled = true;
               next.classList.remove("active")
+              arrowNext.classList.remove("active-arrow")
               prev.classList.add("active");
+              arrowPrev.classList.add("active-arrow");
             },
             reachBeginning() {
               prev.disabled = true;
               prev.classList.remove("active")
+              arrowPrev.classList.remove("active-arrow")
               next.classList.add("active");
+              arrowNext.classList.add("active-arrow")
             },
             fromEdge() {
               next.disabled = false;
               next.classList.add("active");
+              arrowNext.classList.add("active-arrow")
               prev.disabled = false;
               prev.classList.add("active");
+              arrowPrev.classList.add("active-arrow")
             },
         },
 });
 
 next.classList.add("active");
+arrowNext.classList.add("active-arrow")
 
 next.addEventListener('click', () => {
   swiper.slideNext();
