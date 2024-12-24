0ca11098cee3bcc4537021813e480696476aaf35
Olena_Chovgun <olenkachovgun@gmail.com>
2024-12-24 20:39:32 +0200

    add switcher

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,100 +1,4 @@
-// const themeSwitcher = document.getElementById('theme-switcher');
-
-// themeSwitcher.addEventListener('change', () => {
-//   const root = document.documentElement;
-//   switch (themeSwitcher.value) {
-//     case 'pink':
-//       root.style.setProperty('--pink-color', '#ed3b44');
-//       root.style.setProperty('--pink-color-hover', '#e0373f');
-//       root.style.setProperty(
-//         '--background-hero-mobile',
-//         'url(../img/Others/Other_mob/Hero_mob.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-mobile-2x',
-//         'url(../img/Others/Other_mob/Hero@2x_mob.png)'
-//       );
-
-//       root.style.setProperty(
-//         '--background-hero-tablet',
-//         'url(../img/Others/Other_tab/Hero_tab.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-tablet-2x',
-//         'url(../img/Others/Other_tab/Hero@2x_tab.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop',
-//         'url(../img/Others/Other_desk/Hero.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop-2x',
-//         'url(../img/Others/Other_desk/Hero@2x.png)'
-//       );
-//       break;
-//     case 'green':
-//       root.style.setProperty('--pink-color', '#c6e327');
-//       root.style.setProperty('--pink-color-hover', '#8A9B24');
-
-//       root.style.setProperty(
-//         '--background-hero-mobile',
-//         'url(../img/Others/test_background_green/background_green_mob-min.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-mobile-2x',
-//         'url(../img/Others/test_background_green/background_green_mob-min.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-tablet',
-//         'url(../img/Others/test_background_green/background_green_tab-min.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-tablet-2x',
-//         'url(../img/Others/test_background_green/background_green_tab-min.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop',
-//         'url(../img/Others/test_background_green/background_green_desk-min.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop-2x',
-//         'url(../img/Others/test_background_green/background_green_desk-min.png)'
-//       );
-//       break;
-//     case 'blue':
-//       root.style.setProperty(
-//         '--background-hero-mobile',
-//         'url(../img/Others/test_background_blue/Hero_blue.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-mobile-2x',
-//         'url(../img/Others/test_background_blue/Hero_blue@2x.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-tablet',
-//         'url(../img/Others/test_background_blue/Hero_blue@2x.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-tablet-2x',
-//         'url(../img/Others/test_background_blue/Hero_blue@2x.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop',
-//         'url(../img/Others/test_background_blue/Hero_blue@2x.png)'
-//       );
-//       root.style.setProperty(
-//         '--background-hero-desktop-2x',
-//         'url(../img/Others/test_background_blue/Hero_blue@2x.png)'
-//       );
-//       root.style.setProperty('--pink-color', '#0041e8');
-//       root.style.setProperty('--pink-color-hover', '#012EA1');
-//       break;
-//   }
-// });
-
-//
-
-const themeSwitcher = document.getElementById('theme-switcher');
+// ---------------
 
 const themes = {
   pink: {
@@ -142,18 +46,7 @@ const themes = {
       'url(../img/Others/test_background_blue/Hero_blue@2x.png)',
   },
 };
-
-// themeSwitcher.addEventListener('change', () => {
-//   const root = document.documentElement;
-//   const selectedTheme = themes[themeSwitcher.value];
-
-//   if (selectedTheme) {
-//     Object.keys(selectedTheme).forEach(key => {
-//       root.style.setProperty(key, selectedTheme[key]);
-//     });
-//   }
-// });
-
+const themeSwitcher = document.getElementById('theme-switcher');
 const applyTheme = theme => {
   const root = document.documentElement;
   const selectedTheme = themes[theme];
@@ -170,3 +63,70 @@ document.addEventListener('DOMContentLoaded', () => {
 themeSwitcher.addEventListener('change', () => {
   applyTheme(themeSwitcher.value);
 });
+
+// -----------------------custom-select--
+// const customSelect = document.querySelector('.custom-select');
+// const applyTheme = theme => {
+//   const root = document.documentElement;
+//   const selectedTheme = themes[theme];
+//   if (selectedTheme) {
+//     Object.keys(selectedTheme).forEach(key => {
+//       root.style.setProperty(key, selectedTheme[key]);
+//     });
+//   }
+// };
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   applyTheme('pink');
+// });
+
+// customSelect.addEventListener('click', () => {
+//   const optionsContainer = customSelect.querySelector('.options-container');
+//   const optionsList = optionsContainer.querySelectorAll('.option');
+
+//   optionsList.forEach(option => {
+//     option.addEventListener('click', () => {
+//       const value = option.getAttribute('data-value');
+//       const selectedOption = customSelect.querySelector('.selected');
+//       selectedOption.innerText = option.innerText;
+//       selectedOption.setAttribute('data-value', value);
+//       applyTheme(value);
+//     });
+//   });
+// });
+
+// ---
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const customSelect = document.querySelector('.custom-select');
+//   const optionsContainer = document.querySelector('.options-container');
+//   const selectedOption = document.querySelector('.selected');
+//   const optionsList = document.querySelectorAll('.option');
+
+//   customSelect.addEventListener('click', function (event) {
+//     customSelect.classList.toggle('open');
+//     event.stopPropagation();
+//   });
+
+//   optionsList.forEach(option => {
+//     option.addEventListener('click', function () {
+//       selectedOption.innerText = this.innerText;
+//       customSelect.classList.remove('open');
+//     });
+
+//     option.addEventListener('mouseover', function () {
+//       this.style.backgroundColor = 'var(--pink-color-hover)';
+//     });
+
+//     option.addEventListener('mouseout', function () {
+//       this.style.backgroundColor = '';
+//     });
+//   });
+
+//   document.addEventListener('click', function (e) {
+//     if (!customSelect.contains(e.target)) {
+//       customSelect.classList.remove('open');
+//     }
+//   });
+// });
+//
