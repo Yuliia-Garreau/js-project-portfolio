1486dfc609534714b98a95a390e468b497acd9f6
MAKCIMELIANO <makcimeliano@gmail.com>
2024-12-26 22:18:00 +0200

    finish reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,14 +1,13 @@
 import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.css'; // Импортируем все стили Swiper
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css/navigation'; // Импортируем стили для модуля Navigation
-import 'swiper/css/pagination'; // Импортируем стили для модуля Pagination
+import 'swiper/swiper-bundle.css';
+// import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const API_URL = 'https://portfolio-js.b.goit.study/api/reviews';
-
 const reviewsList = document.querySelector('.reviews-list');
 
 async function fetchReviews() {
@@ -32,86 +31,17 @@ function renderReviews(reviews) {
   const reviewsHtml = reviews
     .map(
       review => `
-        <div class="swiper-slide review-slide">
-          <li class="review-item" id="${review._id}">
-            <div class="review-author">
-              <img src="${review.avatar_url}" alt="Avatar of ${review.author}" class="review-avatar"/>
-              <h2 class="review-author-name">${review.author}</h2>
-            </div>
-            <p class="review-text">${review.review}</p>
-          </li>
-        </div>
+        <li class="review-item swiper-slide" id="${review._id}">
+          <div class="review-author">
+            <img src="${review.avatar_url}" alt="Avatar of ${review.author}" class="review-avatar"/>
+            <h2 class="review-author-name">${review.author}</h2>
+          </div>
+          <p class="review-text">${review.review}</p>
+        </li>
       `
     )
     .join('');
   reviewsList.innerHTML = reviewsHtml;
-}
-
-function initializeSwiper() {
-  const swiper = new Swiper('.swiper-container', {
-    modules: [Navigation, Pagination],
-    loop: false, // Отключаем бесконечный цикл
-    slidesPerView: 4, // По умолчанию показывать 4 слайда
-    slidesPerGroup: 1, // Переключаем по одному слайду
-    spaceBetween: 16, // Расстояние между слайдами
-    navigation: {
-      nextEl: '.reviews-swiper-button-next',
-      prevEl: '.reviews-swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      // when window width is >= 1440px
-      1440: {
-        slidesPerView: 4,
-        slidesPerGroup: 1, // Переключаем по одному слайду
-        spaceBetween: 16,
-      },
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 2,
-        slidesPerGroup: 1, // Переключаем по одному слайду
-        spaceBetween: 16,
-      },
-      // when window width is >= 375px
-      375: {
-        slidesPerView: 1,
-        slidesPerGroup: 1, // Переключаем по одному слайду
-        spaceBetween: 16,
-      },
-    },
-    on: {
-      slideChange: function () {
-        updateNavigationButtons(swiper);
-        updateSlideVisibility(swiper);
-      },
-      resize: function () {
-        updateSlideVisibility(swiper);
-      },
-    },
-  });
-
-  // Обновляем состояние кнопок навигации и видимость слайдов при инициализации
-  updateNavigationButtons(swiper);
-  updateSlideVisibility(swiper);
-}
-
-function updateSlideVisibility(swiper) {
-  const slides = swiper.slides;
-  const activeIndex = swiper.activeIndex;
-  const slidesPerView = swiper.params.slidesPerView;
-
-  slides.forEach((slide, index) => {
-    if (index >= activeIndex && index < activeIndex + slidesPerView) {
-      // Отображаем слайды, которые входят в текущий slidesPerView
-      slide.classList.remove('hidden-slide');
-    } else {
-      // Скрываем слайды, которые выходят за рамки текущего slidesPerView
-      slide.classList.add('hidden-slide');
-    }
-  });
 }
 
 function updateNavigationButtons(swiper) {
@@ -133,6 +63,49 @@ function updateNavigationButtons(swiper) {
     prevButton.classList.remove('disabled');
     prevButton.classList.add('active');
   }
+}
+
+function initializeSwiper() {
+  const swiper = new Swiper('.swiper', {
+    // modules: [Navigation, Pagination],
+    loop: false,
+    slidesPerView: 1,
+    loopedSlides: 6,
+    slidesPerGroup: 1,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.reviews-swiper-button-next',
+      prevEl: '.reviews-swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1440: {
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        spaceBetween: 16,
+      },
+      375: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 16,
+      },
+    },
+    on: {
+      slideChange: function () {
+        updateNavigationButtons(swiper);
+      },
+    },
+  });
+
+  updateNavigationButtons(swiper);
 }
 
 document.addEventListener('DOMContentLoaded', fetchReviews);
