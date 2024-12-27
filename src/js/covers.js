6ce1001922ab08943f9665b7636a482fe7ef391e
Roman Soltan <romabandit88@gmail.com>
2024-12-27 02:07:09 +0200

    add start and stop animation by click on section covers

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,11 +1,21 @@
 const section = document.querySelector('.covers');
 const items = section.querySelectorAll('.covers-item');
+section.addEventListener('click', handleClick);
+
+// start and stop animation by click
+function handleClick() {
+  items.forEach(item => {
+    !item.classList.contains('start')
+      ? item.classList.add('start')
+      : item.classList.remove('start');
+  });
+}
 
 function createObserver() {
   const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.1,
+    threshold: 0.2,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
